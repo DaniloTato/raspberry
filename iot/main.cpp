b7743ae5045805c7c0019e44d32ecf132757b714
DaniloTato <Noentiendo2005@gmail.com>
2024-11-17 13:48:51 -0600

    post test merge

--- iot/main.cpp
+++ iot/main.cpp
@@ -47,6 +47,7 @@ typedef bool (*function_ptr)(sf::RenderWindow&, sql::Connection*);
 
 int main(){
     //mysql connection
+    //
 
     sql::mysql::MySQL_Driver* driver = sql::mysql::get_mysql_driver_instance();
     sql::Connection* conn = driver->connect("tcp://127.0.0.1:3306", "root", "");
