73d82dbe3ffe4a062973a5444a87c6b1bea2260c
WhatsNewsYoyo <garciayael715@gmail.com>
2024-11-26 14:02:29 -0600

    Gpios para 4 LEDS y 4 botones. Adicionalmente de funcionamiento de LDR

--- gpio_config.h
+++ gpio_config.h
@@ -3,16 +3,17 @@
 
 #include <wiringPi.h>
 
-// GPIO para botones
+// GPIOs para botones
 const int BUTTON1 = 5;
 const int BUTTON2 = 6;
 const int BUTTON3 = 13;
 const int BUTTON4 = 19;
 
-// GPIO para LEDs
+// GPIOs para LEDs
 const int LED1 = 14;
 const int LED2 = 15;
 const int LED3 = 18;
+const int LED3 = 22;
 
 // Umbral para LDR
 const int LDR_THRESHOLD = 700;
@@ -28,11 +29,14 @@ inline void configureGPIO() {
     pinMode(LED1, OUTPUT);
     pinMode(LED2, OUTPUT);
     pinMode(LED3, OUTPUT);
+    pinModer(LED4, OUTPUT)
 
     //Inicialización de LEDs
     digitalWrite(LED1, LOW);
     digitalWrite(LED2, LOW);
     digitalWrite(LED3, LOW);
+    digitalWrite(LED4, LOW);
+
 }
 
 void leds();
--- ldr_mcp3008.cpp
+++ ldr_mcp3008.cpp
@@ -39,10 +39,14 @@ void leds() {
             digitalWrite(LED1, HIGH);
             digitalWrite(LED2, HIGH);
             digitalWrite(LED3, HIGH);
+            digitalWrite(LED4, HIGH);
+
         } else {
             digitalWrite(LED1, LOW);
             digitalWrite(LED2, LOW);
             digitalWrite(LED3, LOW);
+            digitalWrite(LED3, LOW);
+
         }
 
         cout << "Valor LDR: " << ldrValue << endl;
